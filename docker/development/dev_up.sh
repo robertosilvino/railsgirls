bfcdf45e20cf632b9a5bcf5045671f823ad734fd
Roberto Cunha <robertosilvino@gmail.com>
2018-10-26 02:19:58 -0300

    mysql service

--- docker/development/dev_up.sh
+++ docker/development/dev_up.sh
@@ -2,5 +2,6 @@
 
 cd ..
 #export DOCKERGID=$(cut -d: -f3 < <(getent group docker))
+export DB_HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 docker-compose -f docker-compose.yml up web
 cd util
